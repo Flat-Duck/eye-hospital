e1d9084f24a580a48dd65282d5116713dd239d59
A.Mahidwei <xile.meko@gmail.com>
2024-03-30 21:38:16 +0200

    fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,14 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Adding an admin user
-        // $user = \App\Models\User::factory()
-        //     ->count(1)
-        //     ->create([
-        //         'email' => 'admin@admin.com',
-        //         'password' => \Hash::make('admin'),
-        //     ]);
-   //     $this->call(PermissionsSeeder::class);
+        Adding an admin user
+        $user = \App\Models\User::factory()
+            ->count(1)
+            ->create([
+                'email' => 'admin@admin.com',
+                'password' => \Hash::make('admin'),
+            ]);
+       $this->call(PermissionsSeeder::class);
 
         $this->call(DiagnoseSeeder::class);
         $this->call(HospitalSeeder::class);
